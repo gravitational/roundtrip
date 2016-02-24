1e127d6a6a68d7a54a6f7f079a286b5caac404a7
klizhentas <klizhentas@gmail.com>
2016-02-24 10:29:24 -0800

    add support for cookies

--- client.go
+++ client.go
@@ -48,6 +48,7 @@ import (
 	"strings"
 )
 
+// ClientParam specifies functional argument for client
 type ClientParam func(c *Client) error
 
 // HTTPClient is a functional parameter that sets the internal
@@ -75,6 +76,14 @@ func BearerAuth(token string) ClientParam {
 	}
 }
 
+// CookieJar sets http cookie jar for this client
+func CookieJar(jar http.CookieJar) ClientParam {
+	return func(c *Client) error {
+		c.jar = jar
+		return nil
+	}
+}
+
 // Client is a wrapper holding HTTP client. It hold target server address and a version prefix,
 // and provides common features for building HTTP client wrappers.
 type Client struct {
@@ -84,9 +93,10 @@ type Client struct {
 	v string
 	// client is a private http.Client instance
 	client *http.Client
-
 	// auth tells client to use HTTP auth on every request
 	auth fmt.Stringer
+	// jar is a set of cookies passed with requests
+	jar http.CookieJar
 }
 
 // NewClient returns a new instance of roundtrip.Client, or nil and error
@@ -106,6 +116,9 @@ func NewClient(addr, v string, params ...ClientParam) (*Client, error) {
 		if err := p(c); err != nil {
 			return nil, err
 		}
+	}
+	if c.jar != nil {
+		c.client.Jar = c.jar
 	}
 	return c, nil
 }
@@ -288,7 +301,12 @@ func (c *Client) RoundTrip(fn RoundTripFn) (*Response, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Response{code: re.StatusCode, headers: re.Header, body: buf}, nil
+	return &Response{
+		code:    re.StatusCode,
+		headers: re.Header,
+		body:    buf,
+		cookies: re.Cookies(),
+	}, nil
 }
 
 // SetAuthHeader sets client's authorization headers if client
@@ -310,6 +328,12 @@ type Response struct {
 	code    int
 	headers http.Header
 	body    *bytes.Buffer
+	cookies []*http.Cookie
+}
+
+// Cookies returns a list of cookies set by server
+func (r *Response) Cookies() []*http.Cookie {
+	return r.cookies
 }
 
 // Code returns HTTP response status code
